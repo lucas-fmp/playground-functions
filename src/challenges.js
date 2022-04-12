65cdc20248acebf9b1403eb816ad7f741c1369e2
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-12 18:35:43 -0300

    Requisitos 9 e 10

--- src/challenges.js
+++ src/challenges.js
@@ -84,19 +84,55 @@ function fizzBuzz(numbers) {
   }
   return newArray;
 }
-console.log(fizzBuzz([3, 3, 30, 6, 20, 7]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let encoder = '';
+  encoder = string.replace(/a/g, 1);
+  encoder = encoder.replace(/e/g, 2);
+  encoder = encoder.replace(/i/g, 3);
+  encoder = encoder.replace(/o/g, 4);
+  encoder = encoder.replace(/u/g, 5);
+  return encoder;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let decoder = '';
+  decoder = string.replace(/1/g, 'a');
+  decoder = decoder.replace(/2/g, 'e');
+  decoder = decoder.replace(/3/g, 'i');
+  decoder = decoder.replace(/4/g, 'o');
+  decoder = decoder.replace(/5/g, 'u');
+  return decoder;
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
+// eslint-disable-next-line max-lines-per-function
+function techList(techNames, name) {
+  let object = [];
+  for (let i = 0; i < techNames.length; i += 1) {
+    object.push(
+      {
+        tech: techNames[i],
+        name,
+      },
+    );
+  }
+  function compare(a, b) {
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    return 0;
+  }
+  object.sort(compare);
+
+  if (object.length !== 0) {
+    return object;
+  }
+  return 'Vazio!';
 }
 
 module.exports = {
